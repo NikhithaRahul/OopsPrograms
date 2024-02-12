76e3388657d86efd63f26c688e66faca9fdeeaef
DELL <DELL@DESKTOP-I5JEI3M>
2024-02-12 12:24:02 +0530

    first

---
+++ ProjectOOPS/src/main/java/Myprogram/Show.java
@@ -0,0 +1,15 @@
+package Myprogram;
+
+public class Show {
+	public void display()
+	{
+		System.out.println("Hello");
+	}
+
+	public static void main(String[] args) {
+		Show obj=new Show();
+		obj.display();
+
+	}
+
+}
